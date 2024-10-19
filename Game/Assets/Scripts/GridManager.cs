98a2e884baea129d4909b001430ebd6676f4e3aa
LucSanders <lucdanoob07@gmail.com>
2024-10-19 01:51:42 -0700

    project files

---
+++ Game/Assets/Scripts/GridManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(GridMap))]
+[RequireComponent(typeof(Tilemap))]
+public class GridManager : MonoBehaviour
+{
+    Tilemap tilemap;
+    GridMap grid;
+
+    [SerializeField] TileSet tileSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        grid = GetComponent<GridMap>();
+        grid.Init(10, 8);
+        Set(1, 1, 2);
+        Set(1, 2, 2);
+        Set(2, 1, 1);
+        UpdateTileMap();
+    }
+    void UpdateTileMap()
+    {
+        for (int x = 0; x < grid.length; x++)
+        {
+            for (int y = 0; y < grid.height; y++)
+            {
+                UpdateTile(x, y);
+            }
+        }
+    }
+
+    private void UpdateTile(int x, int y)
+    {
+        int tileId = grid.Get(x, y);
+        if (tileId == -1)
+        {
+            return;
+        }
+
+        tilemap.SetTile(new Vector3Int(x,y,0), tileSet.tiles[tileId]);
+    }
+
+    public void Set(int x, int y, int to)
+    {
+        grid.Set(x, y, to);
+        UpdateTile(x, y);
+    }
+}
